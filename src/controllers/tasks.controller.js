f1422fb4c2c4b80bb1ed3f0af7bbf940c118b2f4
Nicolas Miguel <nickmiguel117@gmail.com>
2026-02-14 15:10:32 -0300

    feat Implementação do endpoint POST /task com validação e status 201

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -3,3 +3,21 @@ let tasks = [];
 exports.getTasks = (req, res) => {
     res.json(tasks);
 };
+
+exports.createTask = (req, res) => {
+    const { title } = req.body;
+
+    if (!title || title.trim() === "") {
+        return res.status(400).json({ message: "Title é obrigatório" });
+    }
+
+    const newTask = {
+        id: Date.now(),
+        title,
+        completed: false,
+        createdAt: new Date()
+    };
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+};
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const tasksController = require("../controllers/tasks.controller");
 
 router.get("/", tasksController.getTasks);
+router.post("/", tasksController.createTask);
 
 module.exports = router;
